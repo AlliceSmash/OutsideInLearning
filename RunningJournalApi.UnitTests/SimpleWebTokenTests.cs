74500bf13354a953dfc48a6ed9444e890ac0ec54
sabrina <sabrina@formfire.com>
2015-11-13 14:11:23 -0500

    add parameterized tests

---
+++ RunningJournalApi.UnitTests/SimpleWebTokenTests.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using Xunit;
+
+namespace RunningJournalApi.UnitTests
+{
+    public class SimpleWebTokenTests
+    {
+        [Fact]
+        public void SutIsIteratorOfClaims()
+        {
+            var sut = new SimpleWebToken();
+            Assert.IsAssignableFrom<IEnumerable<Claim>>(sut);
+        }
+
+        [Fact]
+        public void SutYieldsInjectedClaims()
+        {
+            var expected = new[]
+            {
+                new Claim("foo", "bar"),
+                new Claim("baz", "qux"),
+                new Claim("quux", "corge")
+            };
+
+            var sut = new SimpleWebToken(expected);
+            Assert.True(expected.SequenceEqual(sut));
+            Assert.True(
+                expected.Cast<object>().SequenceEqual(
+                    ((IEnumerable)sut).OfType<object>()));
+
+        }
+
+        [Theory]
+        [InlineData(new string[0], "")]
+        [InlineData(new[] { "foo|bar" }, "foo=bar")]
+        [InlineData(new[] { "foo|bar", "baz|qux" }, "foo=bar&baz=qux")]
+        public void ToStringReturnsCorrectResult(string[] keysAndValues, string expected)
+        {
+            var claims = keysAndValues
+                .Select(s => s.Split('|'))
+                .Select(s => new Claim(s[0], s[1]))
+                .ToArray();
+
+            var sut = new SimpleWebToken(claims);
+            var actual = sut.ToString();
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ RunningJournalApi/SimpleWebToken.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+
+namespace RunningJournalApi
+{
+    public class SimpleWebToken : IEnumerable<Claim>
+    {
+        private readonly IEnumerable<Claim> claims;
+
+        public SimpleWebToken(params Claim[] claims)
+        {
+            this.claims = claims;
+        }
+
+        public IEnumerator<Claim> GetEnumerator()
+        {
+            return claims.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        public override string ToString()
+        {
+            return claims.Select(c => string.Format("{0}={1}", c.Type, c.Value))
+                .DefaultIfEmpty(string.Empty)
+                .Aggregate((x, y) => string.Format("{0}&{1}", x, y));
+        }
+
+    }
+}
